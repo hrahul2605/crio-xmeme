12cf913b330ea2d5933355338733190294dfb80f
hrahul2605 <hrahul2605@gmail.com>
2021-02-08 20:59:19 +0530

    -fix: scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,18 @@
-#!bin/bash
+#!/bin/bash
 
-sudo sh -y -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key -y add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo apt-get -y update
 
 sudo apt-get install -y wget ca-certificates build-essential
 
-curl -sL https://deb.nodesource.com/setup_lts.x | sudo -y -E bash -
+curl -sL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y postgresql postgresql-contrib
+
+/usr/lib/postgresql/10/bin/pg_ctl -D /var/lib/postgresql/10/main -l logfile start
 
 sudo systemctl start postgresql.service
 
