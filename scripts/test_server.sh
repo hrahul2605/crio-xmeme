5d1de60c048e8add155af60db5a3dd2068db3cd1
hrahul2605 <hrahul2605@gmail.com>
2021-02-09 07:16:43 +0530

    -fix: test_script curl url

--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -21,7 +21,7 @@ chmod +x sleep.sh
 curl --location --request GET 'http://localhost:8081/memes'
 
 # Execute the POST /memes endpoint using curl
-curl --location --request POST 'http://<Server_URL>/memes' \
+curl --location --request POST 'http://localhost:8081/memes' \
 --header 'Content-Type: application/json' \
 --data-raw '{
 "name": "xyz",
